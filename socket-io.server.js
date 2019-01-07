3e1ab0b33d6787002abdeb94ce4d99165467fbff
Nadeem Ramsing <nadeem.ramsing@gmail.com>
2019-01-07 11:13:14 +0400

    Code refactor: Formatting

--- socket-io.server.js
+++ socket-io.server.js
@@ -4,6 +4,9 @@ module.exports = server => {
     io
         .of('/chat')
         .on('connection', chat => {
-            chat.on('message', (message, clientName) => (console.log('Message from client: ', message, '\nReplying...'), chat.emit('message', `Hi, ${clientName || 'Client'}.`)));
+            chat.on('message', (message, clientName) => (
+                console.log('Message from client: ', message, '\nReplying...'),
+                chat.emit('message', `Hi, ${clientName || 'Client'}.`)
+            ));
         });
 };
